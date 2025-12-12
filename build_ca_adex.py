e6b6c9d51d1e2988ac93e20b34bbe046756c4177
Pooja Babu <p.babu@fz-juelich.de>
2025-12-12 13:06:51 +0100

    Add an alternate version of Ca-AdEx two-comp model

--- build_ca_adex.py
+++ build_ca_adex.py
@@ -2,7 +2,20 @@ import nest
 
 from pynestml.frontend.pynestml_frontend import generate_nest_target
 
-generate_nest_target(input_path="ca_adex.nestml",
-                     target_path="target",
-                     logging_level="INFO",
-                     module_name="ca_adex_module")
+
+def generate_code(with_alt_version=False):
+    if with_alt_version:
+        generate_nest_target(input_path=["ca_adex.nestml", "ca_adex_alt.nestml"],
+                             target_path="target",
+                             logging_level="INFO",
+                             module_name="ca_adex_module")
+    else:
+        generate_nest_target(input_path="ca_adex.nestml",
+                             target_path="target",
+                             logging_level="INFO",
+                             module_name="ca_adex_module")
+
+
+if __name__ == "__main__":
+    with_alt_version = True
+    generate_code(with_alt_version)
